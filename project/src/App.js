4775ea28e96c43b6c4730c3c7774739930c96516
Matheus <matheustassodjamdjian@gmail.com>
2024-10-14 13:47:59 -0300

    data edit page

--- project/src/App.js
+++ project/src/App.js
@@ -4,11 +4,12 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from './components/pages/Projects';
+import Project from "./components/pages/project"
 
 import Container from "./components/layouts/Container";
 import Navbar from "./components/layouts/Navbar"
 import Footer from "./components/layouts/Footer"
-import Projects from './components/pages/Projects';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
          <Route path="/company" element= {< Company />}/>
          <Route path="/contact" element= {< Contact />}/>
          <Route path="newproject" element= {< NewProject />}/>
+         <Route path="/project/:id" element= {< Project />}/>
         </Routes>
       </Container>
       <Footer/>
